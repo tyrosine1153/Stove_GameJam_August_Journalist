857f3587986ade1842217517ecedc6fd8c13bf27
sonohoshi <yd111544@gmail.com>
2021-08-29 03:47:22 +0900

    Make deck system

--- Journalist/Assets/_Scripts/UI/Card.cs
+++ Journalist/Assets/_Scripts/UI/Card.cs
@@ -1,10 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Card : Widget, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    [SerializeField] private Text nameText, descriptionText;
+    [SerializeField] private Image profileImage;
+    
     private bool _isDragable = true;
+
+    private CardData _data;
+    
+    public CardData Data
+    {
+        get => _data;
+        set
+        {
+            _data = value;
+            Show();
+        }
+    }
+
+    public override void Show()
+    {
+        if (Data.cardInfo == CardInfo.Who)
+        {
+            nameText.text = Data.cardContent;
+            descriptionText.text = Data.cardDescription;
+        }
+        else
+        {
+            descriptionText.text = Data.cardContent;
+        }
+    }
     
     public void OnDrag(PointerEventData eventData)
     {
---
+++ Journalist/Assets/_Scripts/UI/Deck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Deck : MonoBehaviour, IDropHandler
+{
+    private HorizontalLayoutGroup _layoutGroup;
+    private void Start()
+    {
+        _layoutGroup = GetComponent<HorizontalLayoutGroup>();
+        
+        var cards = GameManager.Instance.GetCards(CardInfo.How);
+        foreach (var card in cards)
+        {
+            Widget.Create<Card>().Data = card;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        _layoutGroup.spacing = 1;
+        _layoutGroup.spacing = 0;
+    }
+}
