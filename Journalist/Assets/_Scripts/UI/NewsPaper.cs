c00ce084b4d3d1264777da6f644f2da2ff4ac08b
sonohoshi <yd111544@gmail.com>
2021-08-29 03:47:35 +0900

    Make newspaper

--- Journalist/Assets/_Scripts/UI/NewsPaper.cs
+++ Journalist/Assets/_Scripts/UI/NewsPaper.cs
@@ -1,10 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NewsPaper : Widget, IDropHandler
+public class NewsPaper : MonoSingleton<NewsPaper>, IDropHandler
 {
+    public Text text;
+    public CardInfo nowEnterState;
+    public bool endEnter;
+
+    [SerializeField] private string nowHeadline;
+    
+    public string NowHeadline
+    {
+        get => nowHeadline;
+        set
+        {
+            nowHeadline = value;
+            text.text = nowHeadline;
+        }
+    }
+    
     public void OnDrop(PointerEventData eventData)
     {
-        eventData.pointerDrag.transform.position = transform.position;
+        StartCoroutine(CoDirectCard(eventData.pointerDrag.transform));
+        NowHeadline += eventData.pointerDrag.GetComponent<Card>().Data.cardContent;
+
+        endEnter = true;
+    }
+
+    private IEnumerator CoDirectCard(Transform cardTransform)
+    {
+        cardTransform.DOScale(cardTransform.localScale * .5f, .5f);
+        yield return new WaitForSeconds(.5f);
     }
 }
