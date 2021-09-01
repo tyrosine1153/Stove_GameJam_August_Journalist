1207189fcc559cd969646c7ae822ac550e555cf9
choi82354@gmail.com <choi82354@gmail.com>
2021-09-01 19:10:08 +0900

    Fix NewsText
    Change news Update

--- Journalist/Assets/_Scripts/InGameButton.cs
+++ Journalist/Assets/_Scripts/InGameButton.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,13 +9,14 @@ public class InGameButton : MonoBehaviour // 사실과 메모장 상태 변경
 {
     public GameObject Note; // Note 캔버스 불러오기
     public bool State;
-
+    private float timer;
     void Start() // 처음엔 상태를 비활성화로
     {
         State = false;
         Note.SetActive(false);
+        timer = 0;
     }
-
+    
     public void OnClickNotePad()
     {
         AudioManager.Instance.PlaySound("Memo");
@@ -23,12 +26,22 @@ public class InGameButton : MonoBehaviour // 사실과 메모장 상태 변경
             Note.GetComponentInChildren<Text>().text = GameManager.Instance.GetEvent().eventContent;
             State = true;
         }
-        
+
         else if (State) // 활성화 -> 비활성화
         {
-            Note.SetActive(false); 
-            State = false; 
+            Note.SetActive(false);
+            State = false;
         }
     }
-    
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 0.5)
+        {
+            Note.GetComponentInChildren<Text>().text = GameManager.Instance.GetEvent().eventContent;
+            timer = 0;
+        }
+    }
 }
